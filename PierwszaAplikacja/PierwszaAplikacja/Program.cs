89badae9ada87c97350c71ac414e4e3b9e96b805
DaggeR573 <148980754+DaggeR573@users.noreply.github.com>
2024-01-05 00:18:05 +0100

    #

--- PierwszaAplikacja/PierwszaAplikacja/Program.cs
+++ PierwszaAplikacja/PierwszaAplikacja/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic;
+
+var hehe = 25;
+
+int hehe2 = int.MaxValue;
+
+float hehe3 = float.MaxValue;
+
+long hehe4 = long.MaxValue;
+
+char hehe5 = char.MaxValue;
+
+String hehe6 = String.Empty;
+
+byte hehe7 = byte.MaxValue;
+
+double hehe8 = double.MaxValue;
+
+decimal hehe9 = decimal.MaxValue;
+
+bool hehe10 = false;
