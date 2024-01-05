055a204755b852b1626520af8b9f0532874e8999
DaggeR573 <148980754+DaggeR573@users.noreply.github.com>
2024-01-05 23:52:43 +0100

    Zadanie domowe dzień 3

--- PierwszaAplikacja/PierwszaAplikacja/Program.cs
+++ PierwszaAplikacja/PierwszaAplikacja/Program.cs
@@ -1,26 +1,20 @@
-<<<<<<< HEAD
-﻿using Microsoft.VisualBasic;
+string name = "Ewa";
+string gender = "kobieta";
+int age = 30;
 
-var hehe = 25;
+if (gender == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (gender == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
 
-int hehe2 = int.MaxValue;
-
-float hehe3 = float.MaxValue;
-
-long hehe4 = long.MaxValue;
-
-char hehe5 = char.MaxValue;
-
-String hehe6 = String.Empty;
-
-byte hehe7 = byte.MaxValue;
-
-double hehe8 = double.MaxValue;
-
-decimal hehe9 = decimal.MaxValue;
-
-bool hehe10 = false;
-=======
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Zostanę programistą!");
->>>>>>> dd83f71ed2b9904fb3c6e28dbab3fde3cd38e022
+}
